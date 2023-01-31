a6ff3be8dd27cd81c68d93744cde06b8fc01a6c3
JailsonA <maurocv18@gmail.com>
2023-01-31 10:34:13 +0000

    Config e criação do nosso Dashboard Projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,19 +1,28 @@
 import Message from "../layout/Message";
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import styles from "./Projects.module.css";
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
 
 function Projects() {
-
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
   if (location.state) {
-    message = location.state.message
+    message = location.state.message;
   }
 
   return (
-  <div>
-    <h1>Meus Projetos</h1>
-    {message && <Message type="success" msg={message} />}
-  </div>);
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" /> 
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>meus projectos</p>
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
