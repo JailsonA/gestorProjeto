a33c113e92dfd12cd08783ee6f82a51b85257e96
JailsonA <maurocv18@gmail.com>
2023-01-31 11:37:04 +0000

    Fazer o SELECT na BD do nosso API usando GET HTTP para listar todos os projects no Dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,37 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react'
 import styles from "./Projects.module.css";
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from "../project/ProjectCard";
 
 function Projects() {
+  const [projects, setProjects] = useState([])
+  
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    // Para ver o loading
+    
+        fetch('http://localhost:5000/projects', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+          
+  }, [])
 
   return (
     <div className={styles.project_container}>
@@ -19,7 +41,17 @@ function Projects() {
       </div>
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
-        <p>meus projectos</p>
+      {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+            id={project.id}
+            name={project.name}
+            budget={project.budget}
+            category={project.category.name}
+            key={project.id}
+            />
+          ))}
+        
       </Container>
     </div>
   );
---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,29 @@
+import styles from "./ProjectCard.module.css";
+import { Link } from 'react-router-dom'
+
+import { BsPencil, BsFillTrashFill } from "react-icons/bs";
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+  return (
+    <div className={styles.project_card}>
+      <h4>{name}</h4>
+      <p>
+        <span>Orçamento:</span> {budget} €
+      </p>
+      <p className={styles.category_text}>
+        <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+      </p>
+      <div className={styles.project_card_actions}>
+        <Link to={"/project/" + id}>
+          <BsPencil /> Editar
+        </Link>
+        <button onClick="">
+          <BsFillTrashFill />
+          Excluir
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
